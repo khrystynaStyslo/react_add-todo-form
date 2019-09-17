2288576fae5153d687602f88082c560b5896e041
Khrystyna Styslo <khrystyna.styslo@gmail.com>
2019-09-17 19:31:03 +0300

    Updates

---
+++ precache-manifest.58a44def9fe9c63955af62245c45d2d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4852a113dccd7318e9067949f0d1b77",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "1d80901dd0977e312e11",
+    "url": "/react_add-todo-form/static/css/main.e8172f0f.chunk.css"
+  },
+  {
+    "revision": "74d3da8647cfab6a1ef3",
+    "url": "/react_add-todo-form/static/js/2.c55bae82.chunk.js"
+  },
+  {
+    "revision": "1d80901dd0977e312e11",
+    "url": "/react_add-todo-form/static/js/main.142c7ab2.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  }
+]);
